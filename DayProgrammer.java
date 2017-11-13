03bcb590eed0aa7dba645413385678996a7c316e
imsurajpal <33620551+imsurajpal@users.noreply.github.com>
2017-11-13 21:31:23 +0530

    Update DayProgrammer.java

--- DayProgrammer.java
+++ DayProgrammer.java
@@ -2,15 +2,61 @@
 
 import java.util.Scanner;
 
-public class Abhi {
+public class AbhiNew {
 
 
-    static String solve(int year){
-        if( year == 1918 ) return "26.09.1918";
-        if( isLeap( year ) ) return "12.09." + Integer.toString( year );
-        else return "13.09." + Integer.toString( year );
+        public static String solve(int num,int year ) {
+            int i=0;
 
-    }
+            int[] mon = new int[12];
+            mon[0] = 31;
+            mon[1] = 28;
+            mon[2] = 31;
+            mon[3] = 30;
+            mon[4] = 31;
+            mon[5] = 30;
+            mon[6] = 31;
+            mon[7] = 31;
+            mon[8] = 30;
+            mon[9] = 31;
+            mon[10] = 30;
+            mon[11] = 31;
+
+            String ans="";
+
+            for ( i = 0; i < 12; i++){
+                if (num > mon[i]) {
+
+                    if ((i==1) && (isLeap( year))){
+                        num=num-1;
+                    }
+
+
+                    num = num - mon[i];
+                } else {
+                    ans = num + "." +( i+1)+"."+year;
+                    break;
+                }
+        }
+        return ans;
+
+        }
+//            if (num<31)  return (num+".01.2017");
+//            else if (num > 31 && num <= 59)
+//               return ((num - 31)+".02.2017");
+//             else if (num > 59 && num <= 90)
+//                return (num - 59)+".03.2017";
+//             else if (num > 90 && num <= 120)
+//                return (num - 90)+".04.2017";
+//             else if (num > 120 && num <= 151)
+//                return (num - 120)+".05.2017";
+//             else if (num > 151 && num <= 181)
+//                return (num - 151)+".06.2017";
+//             else if (num > 181 && num <= 212)
+//                return (num - 181)+".07.2017";
+//             else return "09.08.2017";
+//
+//
 
     static boolean isLeap( int year ) {
 
@@ -23,7 +69,8 @@ public class Abhi {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int year = in.nextInt();
-        String result = solve(year);
+        int num = in.nextInt();
+         String result = solve(num,year);
         System.out.println(result);
     }
 }
