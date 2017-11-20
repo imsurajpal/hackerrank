87164f7ed665cc3f65475d0ffde0bb1ee1a39e0f
imsurajpal <33620551+imsurajpal@users.noreply.github.com>
2017-11-21 00:07:05 +0530

    Update AppleOrange.java

--- AppleOrange.java
+++ AppleOrange.java
@@ -1,11 +1,8 @@
 https://www.hackerrank.com/challenges/apple-and-orange/problem
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
+import java.util.Scanner;
 
-public class Solution {
+public class AbhiNew {
+
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -15,12 +12,12 @@ public class Solution {
         int b = in.nextInt();
         int m = in.nextInt();
         int n = in.nextInt();
-       int app=0;
+        int app=0;
         int[] apple = new int[m];
         for(int apple_i=0; apple_i < m; apple_i++){
             apple[apple_i] = in.nextInt();
-            if((a+m)>=s && (a+m)<=t){
-                app=app++;
+            if((a+apple[apple_i])>=s && (a+apple[apple_i])<=t){
+                app++;
             }
 
         }
@@ -28,12 +25,17 @@ public class Solution {
         int[] orange = new int[n];
         for(int orange_i=0; orange_i < n; orange_i++){
             orange[orange_i] = in.nextInt();
-            if((b+n)>=s && (b+n)<=t){
-                ora=ora++;
+            if((b+orange[orange_i])>=s && (b+orange[orange_i])<=t){
+                ora++;
             }
 
         }
+        System.out.println(app);
+        System.out.println(ora);
     }
+
 }
+
+
 
 
